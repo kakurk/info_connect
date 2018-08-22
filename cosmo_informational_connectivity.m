51f8c902ccd0eb36c0db81ebc0f81a64bcad4ac9
Kyle Kurkela <kurkela@bc.edu>
2018-08-22 13:09:29 -0400

    Change from Info Conn Toolbox --> Kyles Custom Code

--- cosmo_informational_connectivity.m
+++ cosmo_informational_connectivity.m
@@ -1,16 +1,44 @@
-function [ timecourse ] = cosmo_informational_connectivity( ds_template, ds_timecourse )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ discriminability_MV_timecourse ] = cosmo_informational_connectivity( ds_betas, ds_timecourse, targetCondition, value )
+% Using a leave-one-run-out procedure, return an "informational timecourse"
+%
 
-lag = 4;
+%% Parse input arguments
 
-chunks = unique(ds_template.sa.chunks);
+assert(isequal(unique(ds_betas.sa.chunks), unique(ds_timecourse.sa.chunks)), 'both datasets must have the same number of chunks')
 
-keyboard
+chunks     = unique(ds_betas.sa.chunks);
+discriminability_MV_timecourse = cell(1, length(chunks));
 
-for c = chunks
+
+for ch = chunks'
     
+    % Template Patterns
+    Filt = ~ismember(ds_betas.sa.chunks, ch);
+    ds_template_patterns = cosmo_slice(ds_betas, Filt);
+    ds_template_patterns = cosmo_average_samples(ds_template_patterns, 'split_by', {targetCondition});
+    
+    % Timecourse
+    Filt = ismember(ds_timecourse.sa.chunks, ch);
+    ds_multivariate_timecourse = cosmo_slice(ds_timecourse, Filt);
+    
+    % Target Pattern MV Timecourse
+    Filt              = strcmp(ds_template_patterns.sa.(targetCondition), value);
+    ds_target_pattern = cosmo_slice(ds_template_patterns, Filt);
+    target_pattern_MV_timecourse = cosmo_corr(ds_target_pattern.samples', ds_multivariate_timecourse.samples');
+    target_pattern_MV_timecourse = atanh(target_pattern_MV_timecourse);
+    
+    % Next Highest Alternate Pattern
+    Filt              = ~strcmp(ds_template_patterns.sa.(targetCondition), value);
+    ds_target_pattern = cosmo_slice(ds_template_patterns, Filt);
+    alternate_pattern_MV_timecourse = cosmo_corr(ds_target_pattern.samples', ds_multivariate_timecourse.samples');
+    alternate_pattern_MV_timecourse = max(alternate_pattern_MV_timecourse, [], 1);
+    alternate_pattern_MV_timecourse = atanh(alternate_pattern_MV_timecourse);
+    
+    % 'Discriminability'
+    discriminability_MV_timecourse{ch} = target_pattern_MV_timecourse - alternate_pattern_MV_timecourse;
     
 end
     
+discriminability_MV_timecourse = horzcat(discriminability_MV_timecourse{:});
+
 end
--- mice_encoding_info_conn.m
+++ mice_encoding_info_conn.m
@@ -1,4 +1,4 @@
-function IC = mice_encoding_info_conn(s)
+function mice_encoding_info_conn(s)
 
 %% Add Paths
 % SPM12, CoSMoMVPA, and the Informational Connectivity Toolbox
@@ -12,7 +12,6 @@ addpath('/gsfs0/data/kurkela/Documents/latest_IC_toolbox');
 % bids_path = where the raw BIDS formatted data are
 data_path  = '/gsfs0/data/ritcheym/data/fmri/mice/analysis/encoding/SingleTrialModel_regularmodel';
 inital_spm_model_path = '/gsfs0/data/ritcheym/data/fmri/mice/analysis/encoding/Emotion_and_Contexts';
-bids_path  = '/gsfs0/data/ritcheym/data/fmri/mice/data/sourcedata';
 
 %% Masks
 % full brain and MTL masks
@@ -30,120 +29,106 @@ subjects  = {'sub-s003' , 'sub-s002', 'sub-s023'};
 
 %% Parameters
 % hemodynamic_lag (in TRs).
-hemodynamic_lag     = 4;
-TR                  = 1.5;
-num_of_TRs_per_Sess = 165;
+numberOfROIs        = length(masks);
 
-%% Load Data
+%% Templates
+% In order to perform informational connectivity, we need to define several
+% "template patterns" to correlate with the fMRI timecourse. There should
+% be one "template pattern" per condition, that is defined based on
+% independent training data.
+
+% all of the single trial beta images for this subject
+single_trial_betasFNs = RecurseAndFilterFileSearch(data_path, 'Sess0[1-6].*\.nii', subjects{s});
+
+% Grab all four dimensional beta images. Same as single_trial_betasFNs,
+% just collapsed into a single file. Four dimensional file format required
+% for cosmo_fmri_dataset.m
+fourDsingletrialbetasFN = RecurseAndFilterFileSearch(data_path, '.*all-betas\.nii$', subjects{s});
+assert(length(fourDsingletrialbetasFN) == 1, 'There is more than 1 four D file');
+
+% initalize
+ds_template = cell(1, numberOfROIs);
+
+for m = 1:numberOfROIs
+    
+    % Load the fMRI data into MATLAB using cosmo_fmri_dataset. Assign a new
+    % dataset attribute "maskName" with the name of the mask used to create
+    % it.
+    ds_template{m} = cosmo_fmri_dataset(char(fourDsingletrialbetasFN), 'mask', masks{m});
+    ds_template{m}.a.maskName = masks{m};
+    
+    % using the custom regularExpression, extract the substring from the
+    % single_trial_betaFNs identifying the beta's emotional valence. Take
+    % the match and convert it from a nested cell array to a cell string.
+    % Assign it as a new sample attribute "EmotionalValence".
+    regularExpression = '(?<=Emotion-)[N][a-z]{6,7}(?=_)';
+    matches = regexp(single_trial_betasFNs, regularExpression, 'match');
+    matches = cellfun(@char, matches, 'UniformOutput', false);
+    ds_template{m}.sa.EmotionalValence = matches;
+    
+    % using the custom regularExpression, extract the substring from the
+    % single_trial_betaFNs, identifying the beta's context number. Take the
+    % match and convert it from a nested cell array to a cell string to a 
+    % vector of doubles. Assign this column vector as a new sample
+    % attribute, "ContextNumber".
+    regularExpression = '(?<=Context-)[0-4](?=_)';
+    matches           = regexp(single_trial_betasFNs, regularExpression, 'match');
+    matches           = cellfun(@(x) str2double(cell2mat(x)), matches);
+    ds_template{m}.sa.ContextNumber = matches;
+    
+    % using custom regularExpression, extract the session number from the
+    % single_trial_betaFNs.
+    regularExpression = '(?<=Sess0)[0-6](?=_)';
+    matches           = regexp(single_trial_betasFNs, regularExpression, 'match');
+    matches           = cellfun(@(x) str2double(cell2mat(x)), matches);
+    ds_template{m}.sa.chunks = matches;
+    
+end
+
+%% Timecourse
 % Use SPM12 and CoSMoMVPA tools to get the fmri data into MATLAB
 
 %%% Timecourse
 % Strategy: find the original GLM and figure out the raw timeseries from
-% the information contained within the SPM.mat file.
+% the information contained within the SPM.mat file. Why do it this way? It
+% ensures that we are using the exact same timeseries that were used to
+% creaete the single trial beta images above.
 
-% Find the original SPM.mat file
-dir2search = fullfile(inital_spm_model_path, subjects{s});
-SPM_FN   = spm_select('FPList', dir2search, 'SPM.mat');
-assert(~isempty(SPM_FN), 'Could not find this subject''s original SPM.mat file', subjects{s})
-SPM = [];
-load(SPM_FN)
+% Find the original SPM.mat file and load it into MATLAB
+SPM_FN = RecurseAndFilterFileSearch(inital_spm_model_path, 'SPM.mat', subjects{s});
+assert(length(SPM_FN) == 1, 'More than 1 SPM.mat identified for subject %s', subjects{s})
+SPM    = [];
+load(char(SPM_FN))
 
 % Figure out the raw timeseries
-somefile   = unique(cellfun(@(x) x{:}, regexp(cellstr(SPM.xY.P), '.*(?=,[0-9]{1,3})', 'match'), 'UniformOutput', false));
-timecourse = cell(1,length(somefile));
-
-% All of the BIDS information from MICE
-BIDS = spm_BIDS(bids_path);
-
-% The BIDS information for this subject
-thisSubjectBIDS = BIDS.subjects(strcmp({BIDS.subjects.name}, subjects{s}));
-
-% Just the encoding task
-thisSubjectBIDS = thisSubjectBIDS.func(strcmp({thisSubjectBIDS.func.task}, 'encoding'));
+timeseriesFNs_w_frames = cellstr(SPM.xY.P);
+timeseriesFNs   = regexp(timeseriesFNs_w_frames, '.*(?=,[0-9]{1,3})', 'match');
+timeseriesFNs   = cellfun(@char, timeseriesFNs, 'UniformOutput', false);
+timeseriesFNs   = unique(timeseriesFNs);
+timecourse      = cell(1,length(timeseriesFNs));
 
 % for each mask (i.e., ROI)
-concatenated_timecourse = {};
-for m = 1:length(masks)
-    for c = 1:length(somefile) % for each run
+ds_timecourse = {};
+for m = 1:numberOfROIs
+    for c = 1:length(timeseriesFNs) % for each run
         
-        filt{1} = strcmp(thisSubjectBIDS(c).events.ContextNum, '1');
-        filt{2} = strcmp(thisSubjectBIDS(c).events.ContextNum, '2');
-        filt{3} = strcmp(thisSubjectBIDS(c).events.ContextNum, '3');
-        filt{4} = strcmp(thisSubjectBIDS(c).events.ContextNum, '4');
-        
-        timepointIDs = zeros(num_of_TRs_per_Sess, 1);
-        
-        for f = 1:length(filt)
-            contextNonsetsInTRs = round(thisSubjectBIDS(c).events.onset(filt{f})/TR + hemodynamic_lag);
-            timepointIDs(contextNonsetsInTRs)  = f;
-        end
-        
-        timecourse{c}            = cosmo_fmri_dataset(somefile{c}, 'mask', masks{m}, 'chunks', c);
-        timecourse{c}.sa.targets = timepointIDs;
-        timecourse{c}.sa.roi     = ones(size(timecourse{c}.samples, 1), 1) * m;
-        
-        %%% Blocks
-        blockFilt = strcmp(thisSubjectBIDS(c).events.Condition, 'neg') | ...
-                    strcmp(thisSubjectBIDS(c).events.Condition, 'neu');
-        % onsets
-        blockonsets = thisSubjectBIDS(c).events.onset(blockFilt);
-        
-        % tricky part: durations. figure out the offset of the fourth trial
-        % in each block.
-        fourthTrialInEachBlockIDX = find(blockFilt) + 4;
-        fourthTrialInEachBlockOnsets = thisSubjectBIDS(c).events.onset(fourthTrialInEachBlockIDX);
-        fourthTrialInEachBlockDur    = 3;
-        fourthTrialInEachBlockOffset = fourthTrialInEachBlockOnsets + fourthTrialInEachBlockDur;
-        blockdurs   = fourthTrialInEachBlockOffset - blockonsets;
-        
-        timecourse{c}.sa.blockHeatVec = zeros(165, 1);
-        
-        blockonsetsInTRs = round(blockonsets/TR);
-        blockdursInTRs   = round(blockdurs/TR);
-        
-        blockDurationsAsIDXs = [];
-        blockDurationsVIZ = {};
-        
-        %% A visual
-        
-        for i = 1:length(blockonsetsInTRs)
-            curBlockIDXs = blockonsetsInTRs(i):blockonsetsInTRs(i)+blockdursInTRs(i);
-            blockDurationsAsIDXs = horzcat(blockDurationsAsIDXs, curBlockIDXs);
-            blockDurationsVIZ = vertcat(blockDurationsVIZ, {curBlockIDXs});
-        end
-        
-        VISUAL = zeros(8, 165);
-        
-        for i = 1:length(blockDurationsVIZ)
-           VISUAL(i, blockDurationsVIZ{i}) = 1; 
-        end
-        
-        %figure; imagesc(VISUAL); colorbar;
-        
-        %%
-        
-        timecourse{c}.sa.blockHeatVec(blockDurationsAsIDXs) = 1;
-
+        timecourse{c}            = cosmo_fmri_dataset(timeseriesFNs{c}, ...
+                                                      'mask', masks{m}, ...
+                                                      'chunks', c);
+        timecourse{c}.a.maskName = masks{m};
     end
-    concatenated_timecourse = vertcat(concatenated_timecourse, cosmo_stack(timecourse));
+    ds_timecourse = vertcat(ds_timecourse, cosmo_stack(timecourse));
 end
 
-%% Information Connectivity Toolbox
-% The information connectivity toolbox requries that the data is structured
-% in a very specific way. See run_ROI_IC.m help text for more info.
-IC.data       = vertcat(concatenated_timecourse{1}.samples', concatenated_timecourse{2}.samples'); % rows = voxels, columns = trials
-A = ones(size(concatenated_timecourse{1}.samples, 2), 1);
-B = ones(size(concatenated_timecourse{2}.samples, 2), 1) * 2;
-IC.ROIs       = vertcat(A, B);
-mat = vertcat(concatenated_timecourse{1}.sa.targets' == 1, ...
-              concatenated_timecourse{1}.sa.targets' == 2, ...
-              concatenated_timecourse{1}.sa.targets' == 3, ...
-              concatenated_timecourse{1}.sa.targets' == 4);
-IC.conditions = mat;
-IC.folds      = concatenated_timecourse{1}.sa.chunks';
-IC.selector   = concatenated_timecourse{1}.sa.blockHeatVec';
-IC.ROI_names  = cellfun(@(x) char(regexp(x, 'r[A-Z].*\.nii$', 'match')), masks, 'UniformOutput', false);
+%% Information Connectivity!
 
-IC = run_ROI_IC(IC);
+keyboard
+
+info_timecourse = cell(1, numberOfROIs);
+for m = 1:numberOfROIs
+    info_timecourse{m} = cosmo_information_connectivity(ds_template{m}, ds_timecourse{m});
+end
+
+keyboard
 
 end
